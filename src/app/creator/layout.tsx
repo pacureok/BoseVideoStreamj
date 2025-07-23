7686417b448a4dbed27fc3119205348bcf03dbf4
pacureok <okkpacur@gmail.com>
2025-07-22 23:22:18 -0600

    Update layout.tsx

--- src/app/creator/layout.tsx
+++ src/app/creator/layout.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../api/auth/[...nextauth]/route"; // ¡Ruta relativa!
+import { authOptions } from "../../api/auth/[...nextauth]/route"; // Ruta relativa corregida
 import { redirect } from "next/navigation";
 import React from "react";
 
