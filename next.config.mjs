c703afb3611ad0da40a7059c9ee2f3d59af94bad
pacureok <okkpacur@gmail.com>
2025-07-22 22:36:49 -0600

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Aquí puedes añadir configuraciones específicas de Next.js
+  // Por ejemplo, para habilitar características experimentales o configurar imágenes.
+  // output: 'standalone', // Si necesitas un output de Docker o similar
+  // swcMinify: true, // Habilita la minificación con SWC para mayor velocidad
+};
+
+export default nextConfig;
